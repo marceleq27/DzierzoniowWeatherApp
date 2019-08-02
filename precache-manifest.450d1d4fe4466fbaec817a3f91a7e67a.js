31bcb5a24f6f12b037b593edec238b329b22104c
Marcel Bednarz <marcel.bednarz16@gmail.com>
2019-08-02 23:40:40 +0200

    Updates

---
+++ precache-manifest.450d1d4fe4466fbaec817a3f91a7e67a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23bb7fb8c8a96574e448cfaff060ec4c",
+    "url": "/DzierzoniowWeatherApp/index.html"
+  },
+  {
+    "revision": "1f80580251b0932e0fb7",
+    "url": "/DzierzoniowWeatherApp/static/css/main.7a78a388.chunk.css"
+  },
+  {
+    "revision": "11215573eebc52733c67",
+    "url": "/DzierzoniowWeatherApp/static/js/2.de6abf1c.chunk.js"
+  },
+  {
+    "revision": "1f80580251b0932e0fb7",
+    "url": "/DzierzoniowWeatherApp/static/js/main.59bd93f6.chunk.js"
+  },
+  {
+    "revision": "8e3a3702dc6a0d84aba3",
+    "url": "/DzierzoniowWeatherApp/static/js/runtime~main.6d3fdd40.js"
+  },
+  {
+    "revision": "3c9c99390c0185f83c12be4f0099dd1c",
+    "url": "/DzierzoniowWeatherApp/static/media/lightstorm.3c9c9939.gif"
+  },
+  {
+    "revision": "e9cfa71f02ff8e3504410e831d883cd7",
+    "url": "/DzierzoniowWeatherApp/static/media/sunny.e9cfa71f.gif"
+  }
+]);
